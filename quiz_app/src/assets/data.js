03a36639dc1303a5baa883f0c337ef00153edbf4
kiraa123 <kiranprakash970@gmail.com>
2024-04-22 12:57:03 +0530

    app layout

---
+++ quiz_app/src/assets/data.js
@@ -0,0 +1,21 @@
+export const data=[
+    {
+        question:'Which letter is in between A and B in the alphabetical series?' ,
+        option1: 'A',
+        option2: 'B',
+        option3:'Z',
+        option4:'C',
+        ans :4
+    },
+    {
+        question:'What is the place place of M in the alphabetical series?' ,
+        option1: '1',
+        option2: '12',
+        option3:'13',
+        option4:'26',
+        ans :3
+
+
+    }
+
+]
---
+++ quiz_app/src/components/Quiz/Quiz.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Quiz.css";
+
+import {data} from '../../assets/data'
+
+const Quiz = () => {
+
+    let[index,setIndex]=useState(0)
+    let [question,setQuestion]=useState(data[index])
+
+    const checkAns=(e,ans)=>{
+        if(question.ans===ans){
+            e.target.classList.add('correct')
+        }else{
+            e.target.classList.add('wrong')
+  
+        }
+
+
+    }
+  return(
+
+    <>
+    <div className="container">
+
+        <h1>Quiz App</h1>
+        <hr/>
+        <h2>{index+1}.{question.question}</h2>
+        <ul>
+
+            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 Questions</div>
+    </div>
+  </>
+  ) 
+};
+
+export default Quiz;
